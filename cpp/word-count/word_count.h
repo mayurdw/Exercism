5f1ac279221ecf7c2f5bc424afd8dba0d3260ac4
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2018-07-11 22:32:13 +1200

    word_count

---
+++ cpp/word-count/word_count.h
@@ -0,0 +1,18 @@
+/*
+ * Author: Mayur Wadhwani
+ * Created: July 2018
+*/
+
+#ifndef WORD_COUNT
+#define WORD_COUNT
+
+#include <map>
+
+using namespace std;
+
+namespace word_count 
+{
+    map <string, int> words( const string &csInput );
+};
+
+#endif
