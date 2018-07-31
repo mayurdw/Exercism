d02c0d847af16afba57356142199f8b3e5d8c993
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2018-07-31 17:28:03 +1200

    Adding robot_name files
    Temp commit for the day

---
+++ cpp/robot-name/robot_name.cpp
@@ -0,0 +1,8 @@
+#include "robot_name.h"
+
+using namespace std;
+
+robot_name::robot::robot()
+{
+
+}
---
+++ cpp/robot-name/robot_name.h
@@ -0,0 +1,24 @@
+/* 
+   Author : Mayur Wadhwani
+   Created : July 2018
+*/
+
+#ifndef ROBOT_NAME_H
+#define ROBOT_NAME_H
+
+#include <string>
+
+namespace robot_name
+{
+   class robot
+   {
+   private:
+      std::string sRobotName;
+
+   public:
+      robot();
+      ~robot() = default;
+      std::string const &name() const { return sRobotName; }
+   };
+}
+#endif
