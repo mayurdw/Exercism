bf5103bb193b7dd166717a6eb2f99e46ca47de1a
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2018-08-14 18:44:39 +1200

    Sieve
    Copied

---
+++ cpp/sieve/sieve.h
@@ -0,0 +1,17 @@
+/*
+   Author : Mayur Wadhwani
+   Created: August 2018
+*/
+
+#ifndef SIEVE_H
+#define SIEVE_H
+
+#include <vector>
+
+namespace sieve
+{
+   std::vector<int> primes( int iNum );
+}
+#endif // !SIEVE_H
+
+
