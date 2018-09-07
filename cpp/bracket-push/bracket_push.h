59b7a24736367e8def256c722e87ab4c0fd6efc2
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2018-09-07 18:57:50 +1200

    Last two cpp exercises

---
+++ cpp/bracket-push/bracket_push.h
@@ -0,0 +1,17 @@
+/*
+   Author : Mayur Wadhwani
+   Created: September 2018
+*/
+
+#ifndef BRACKET_H
+#define BRACKET_H
+
+#include <string>
+
+namespace bracket_push
+{
+   bool check( std::string const& expression );
+}
+
+
+#endif // !BRACKET_H
