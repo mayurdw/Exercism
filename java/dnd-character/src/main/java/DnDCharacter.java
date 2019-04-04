04b409c931bc37779421c8861c7daea4c8260e1e
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2019-04-04 18:20:30 +1300

    DnD Character I don't know what this task requires of me??!!!

---
+++ java/dnd-character/src/main/java/DnDCharacter.java
@@ -0,0 +1,39 @@
+class DnDCharacter {
+
+    int ability() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    int modifier(int input) {
+        return 0;
+    }
+
+    int getStrength() {
+        return 0;
+    }
+
+    int getDexterity(){
+        return 0;
+    }
+
+    int getConstitution(){
+        return 0;
+    }
+
+    int getIntelligence(){
+        return 0;
+    }
+
+    int getWisdom(){
+        return 0;
+    }
+
+    int getCharisma(){
+        return 0;
+    }
+
+    int getHitpoints(){
+        return 0;
+    }
+
+}
