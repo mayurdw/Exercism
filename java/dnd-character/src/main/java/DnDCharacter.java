c597edf354e984c0da069aace5155b4f51267958
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2019-04-11 12:36:42 +1200

    DnD Character

--- java/dnd-character/src/main/java/DnDCharacter.java
+++ java/dnd-character/src/main/java/DnDCharacter.java
@@ -1,39 +1,62 @@
-class DnDCharacter {
+import java.util.Arrays;
+import java.util.Random;
 
+class DnDCharacter {
+    private int ability = AssignPoints();
+    private int strength = AssignPoints();
+    private int dexterity = AssignPoints();
+    private int constitution = AssignPoints();
+    private int intelligence = AssignPoints();
+    private int wisdom = AssignPoints();
+    private int charisma = AssignPoints();
+    private int HitPoints = 10 + this.modifier(this.constitution);
+
+    private int AssignPoints(){
+        int[] diceNumber = new int[4];
+        Random dice = new Random();
+
+        for ( int i = 0; i < diceNumber.length; i++ ){
+            // random number generator code from https://stackoverflow.com/questions/5887709/getting-random-numbers-in-java
+            diceNumber[i] = dice.nextInt((6-1) + 1) + 1;
+        }
+
+        Arrays.sort(diceNumber);
+        return diceNumber[1] + diceNumber[2] + diceNumber[3];
+    }
     int ability() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.ability;
     }
 
     int modifier(int input) {
-        return 0;
+        return Math.floorDiv( (input - 10 ), 2 );
     }
 
     int getStrength() {
-        return 0;
+        return this.strength;
     }
 
     int getDexterity(){
-        return 0;
+        return this.dexterity;
     }
 
     int getConstitution(){
-        return 0;
+        return this.constitution;
     }
 
     int getIntelligence(){
-        return 0;
+        return this.intelligence;
     }
 
     int getWisdom(){
-        return 0;
+        return this.wisdom;
     }
 
     int getCharisma(){
-        return 0;
+        return this.charisma;
     }
 
     int getHitpoints(){
-        return 0;
+        return this.HitPoints;
     }
 
 }
