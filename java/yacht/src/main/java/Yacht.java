58e0905b056232bd9fdcdc63b4e9e65114666d9c
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2019-05-21 19:12:39 +1200

    Yacht

--- java/yacht/src/main/java/Yacht.java
+++ java/yacht/src/main/java/Yacht.java
@@ -1,46 +1,114 @@
 import java.util.Arrays;
-import java.util.List;
 
 class Yacht {
     private int[] dice;
     private YachtCategory yachtCategory;
+
     Yacht(int[] dice, YachtCategory yachtCategory) {
+
         this.dice = dice;
         Arrays.sort(this.dice);
         this.yachtCategory = yachtCategory;
     }
 
-    int getMatches(){
-        return 0;
+    private int getSum(){
+        int Sum = 0;
+
+        for( int i : this.dice){
+            Sum += i;
+        }
+
+        return Sum;
     }
 
-    int score() {
-        int Score = 0;
-        int MaxMatch = 0;
+    private int getMatches( int MatchTo ){
         int Count = 0;
 
-        for( int i = 0; i < this.dice.length - 1; i++ ){
-            if( this.dice[i] == this.dice[i+1]){
+        for( int i : this.dice ){
+            if( i == MatchTo ){
                 Count++;
-            }
-            else{
-                MaxMatch = Count + 1;
-                Count = 0;
             }
         }
 
-        MaxMatch = Count + 1;
-        switch (MaxMatch){
-            case 5: {
-                if (this.yachtCategory == YachtCategory.YACHT) {
-                    Score = 50;
-                } else {
-                    Score = 0;
-                }
-                break;
+        return Count;
+    }
+
+    private int getMatches( ){
+        int MaxMatch = 0;
+        int Count = 0;
+
+
+        for( int i = 0; i < this.dice.length - 1; i++ ){
+            if( this.dice[i] == this.dice[i+1] ){
+                Count++;
+            }
+            else{
+                Count = 0;
             }
 
-            default:
+            if( MaxMatch < Count + 1 ){
+                MaxMatch = Count + 1;
+            }
+        }
+
+        return MaxMatch;
+    }
+    int score() {
+        int Score = 0;
+
+        switch (this.yachtCategory){
+            case YACHT:
+                Score = ( this.getMatches( ) == 5 ) ? 50 : 0;
+                break;
+            case ONES:
+                Score = this.getMatches(1);
+                break;
+            case TWOS:
+                Score = 2 * this.getMatches(2);
+                break;
+            case FOURS:
+                Score = 4 * this.getMatches(4);
+                break;
+            case THREES:
+                Score = 3 * this.getMatches(3);
+                break;
+            case FIVES:
+                Score = 5 * this.getMatches(5);
+                break;
+            case SIXES:
+                Score = 6 * this.getMatches(6);
+                break;
+            case FULL_HOUSE:{
+                // Three of one kind, two of the other
+                int OneKind = getMatches();
+                if( OneKind == 3 || OneKind == 2 ){
+                    int OtherKind = getMatches( this.dice[this.dice.length - 1] );
+                    if( OtherKind == 2 || OtherKind == 3 ){
+                        Score = this.getSum();
+                    }
+                }
+            }
+            break;
+            case FOUR_OF_A_KIND:{
+                if( this.getMatches() >= 4 ){
+                    int Number = ( this.getMatches(this.dice[0] ) == 4 ) ? this.dice[0] : this.dice[this.dice.length - 1];
+                    Score = 4 * Number;
+                }
+            }
+            break;
+            case LITTLE_STRAIGHT:{
+                if( Arrays.equals(this.dice, new int[]{1,2,3,4,5})){
+                    Score = 30;
+                }
+            }
+            case BIG_STRAIGHT:{
+                if( Arrays.equals(this.dice,new int[]{2,3,4,5,6})){
+                    Score = 30;
+                }
+            }
+            break;
+            case CHOICE:
+                Score = this.getSum();
                 break;
         }
 
