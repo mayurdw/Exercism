7eff8c635f9fe0f2293012f0b4eaa4aa5a7d3e26
mayurdw <mayurwadhwanid@gmail.com>
2019-05-23 22:36:40 +1200

    Handshake completed

--- java/secret-handshake/src/main/java/HandshakeCalculator.java
+++ java/secret-handshake/src/main/java/HandshakeCalculator.java
@@ -3,74 +3,36 @@ import java.util.Collections;
 import java.util.List;
 
 class HandshakeCalculator {
-    private List<Signal> Handshake = new ArrayList<>();
 
     List<Signal> calculateHandshake(int number) {
- /*       int Position = 1;
-        while( number != 0 ){
-            if( ( number & 1 ) == 1 ){
-                switch (Position){
-                    case 1:
-                        this.Handshake.add(Signal.WINK);
-                        break;
-                    case 3:
-                        this.Handshake.add(Signal.DOUBLE_BLINK);
-                        break;
-                    case 5:
-                        this.Handshake.add(Signal.CLOSE_YOUR_EYES);
-                        break;
+        List<Signal> Handshake = new ArrayList<>();
+        final int INCLUDE_WINK = 1;
+        final int INCLUDE_DOUBLE_BLINK = 2;
+        final int INCLUDE_CLOSE_YOUR_EYES = 4;
+        final int INCLUDE_JUMP = 8;
+        final int REVERSE_HANDSHAKE = 16;
 
-                }
-            }
-            Position++;
-            number = number >>> 1;
+        if ( ( number & INCLUDE_WINK ) == INCLUDE_WINK ){
+            Handshake.add( Signal.WINK );
         }
-*/
-        /*if( number % 2 == 1 ){
-            this.Handshake.add(Signal.WINK);
 
-            if( number >= 32){
-                // Remove excess
-                number -= 16;
-            }
-
-            if( number >= 3 ){
-                this.Handshake.add(Signal.DOUBLE_BLINK);
-            }
-
-            if(number >= 5 && number % 5 == 0 ){
-                this.Handshake.add(Signal.CLOSE_YOUR_EYES);
-            }
-
-            if(number >= 15 && number % 15 == 0){
-                this.Handshake.add(Signal.JUMP);
-            }
-
-            if( number >= 16 ){
-                Collections.reverse(this.Handshake);
-            }
+        if( ( number & INCLUDE_DOUBLE_BLINK ) == INCLUDE_DOUBLE_BLINK ){
+            Handshake.add( Signal.DOUBLE_BLINK );
         }
-        else {
-            switch( number / 2 ){
-                case 1:
-                    this.Handshake.add(Signal.DOUBLE_BLINK);
-                    break;
-                case 2:
-                    this.Handshake.add(Signal.CLOSE_YOUR_EYES);
-                    break;
-                case 12:
-                case 4:
-                    this.Handshake.add(Signal.JUMP);
-                    break;
-                case 8:
-                    Collections.reverse(this.Handshake);
-                default:
-                    break;
-            }
 
-        }*/
+        if( ( number & INCLUDE_CLOSE_YOUR_EYES ) == INCLUDE_CLOSE_YOUR_EYES ){
+            Handshake.add( Signal.CLOSE_YOUR_EYES );
+        }
 
-        return this.Handshake;
+        if( ( number & INCLUDE_JUMP ) == INCLUDE_JUMP ){
+            Handshake.add( Signal.JUMP );
+        }
+
+        if( ( number & REVERSE_HANDSHAKE ) == REVERSE_HANDSHAKE ){
+            Collections.reverse( Handshake );
+        }
+
+        return Handshake;
     }
 
 }
