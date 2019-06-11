6b03136cddbc45e165762302a52fa06c8c41ecd8
mayurdw <mayurwadhwanid@gmail.com>
2019-06-11 22:39:09 +1200

    Incomplete solution

--- java/matrix/src/main/java/Matrix.java
+++ java/matrix/src/main/java/Matrix.java
@@ -1,31 +1,56 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 class Matrix {
     private String matrixAsString;
-    private int numberOfRows = 1;
+    private List<int[]> matrixRows = new ArrayList<>();
+    private List<int[]> matrixColumms = new ArrayList<>();
+    private int numberOfRows = 0;
+    private int numberOfColumns = 0;
 
     Matrix(String matrixAsString) {
-        int index = 0;
         this.matrixAsString = matrixAsString;
-        while ((index = this.matrixAsString.indexOf('\n', index)) != -1) {
-            this.numberOfRows++;
+        this.populateMatrixRows();
+        this.getNumberOfColumns();
+        this.populateMatrixColumns();
+    }
+
+    private void populateMatrixRows(){
+        String[] matrixRows = this.matrixAsString.split("\n");
+
+        this.numberOfRows = matrixRows.length;
+        for( String s : matrixRows ){
+            String[] rowString = s.split(" ");
+            int[] rowInt = new int[rowString.length];
+            int index = 0;
+
+            for( String s2 : rowString ){
+                rowInt[index++] = Integer.parseInt(s2);
+            }
+
+            this.matrixRows.add(rowInt);
         }
+    }
+
+    private void getNumberOfColumns(){
+        this.numberOfColumns = 1;
+
+        if( this.matrixAsString.contains("\n")){
+            String copy = this.matrixAsString.replace(" ", "");
+
+            this.numberOfColumns = copy.indexOf("\n");
+        }
+    }
+
+    private void populateMatrixColumns(){
     }
 
     int[] getRow(int rowNumber) {
-        int[] rows = new int[this.numberOfRows];
-
-        for (int i = 0; i < this.numberOfRows; i++) {
-            String[] subString = this.matrixAsString.split(" ");
-
-            for( String s : subString ){
-                rows[i] = Integer.parseInt(s);
-            }
-        }
-
-        return rows;
+        return this.matrixRows.get(rowNumber-1);
     }
 
     int[] getColumn(int columnNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.matrixColumms.get(columnNumber - 1);
     }
 }
