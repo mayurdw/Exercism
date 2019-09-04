ac9135d0c619f9d95d0f5729957bacc3525cc31b
mayurdw <mayurwadhwanid@gmail.com>
2019-09-04 23:39:27 +1200

    Mentor's feedback changes: Might revert it later

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -1,32 +1,28 @@
+import java.util.Arrays;
+
 class Triangle {
-    private double side1,side2,side3;
+    private double[] sides = new double[3];
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
-        if( this.areSidesValid(side1,side2,side3)) {
-            this.side1 = side1;
-            this.side2 = side2;
-            this.side3 = side3;
-        } else {
+        this.sides[0] = side1;
+        this.sides[1] = side2;
+        this.sides[2] = side2;
+
+        Arrays.sort(this.sides);
+        if( this.sides[0]<=0) {
             throw new TriangleException("Illegal args");
         }
     }
 
-    private boolean areSidesValid(double side1, double side2, double side3){
-        return ( ( side1 != 0 && side2 != 0 && side3 != 0 ) &&
-                ( side1 + side2 >= side3 ) &&
-                ( side1 + side3 >= side2 ) &&
-                ( side2 + side3 >= side1 ));
-    }
-
     boolean isEquilateral() {
-        return this.side1 == this.side2 && this.side3 == this.side2 && this.side3 == this.side1;
+        return this.sides[0] == this.sides[1] && this.sides[1] == this.sides[2];
     }
 
     boolean isIsosceles() {
         return this.isEquilateral() ||
-                ( this.side3 == this.side2 && this.side3 != this.side1 ) ||
-                ( this.side2 == this.side1 && this.side2 != this.side3 ) ||
-                ( this.side1 == this.side3 && this.side1 != this.side2 );
+                ( this.sides[2] == this.sides[1] && this.sides[2] != this.sides[0] ) ||
+                ( this.sides[1] == this.sides[0] && this.sides[1] != this.sides[2] ) ||
+                ( this.sides[0] == this.sides[2] && this.sides[0] != this.sides[1] );
     }
 
     boolean isScalene() {
