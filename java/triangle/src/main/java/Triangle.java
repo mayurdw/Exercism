6f7faed041eddb25c59ca55e7823bdca6ccc4279
mayurdw <mayurwadhwanid@gmail.com>
2019-09-09 21:41:57 +1200

    Feedback changes 2

--- java/triangle/src/main/java/Triangle.java
+++ java/triangle/src/main/java/Triangle.java
@@ -5,20 +5,19 @@ class Triangle {
     private boolean isEquilateral;
 
     Triangle(double side1, double side2, double side3) throws TriangleException {
-        double[] sides = new double[3];
-        sides[0] = side1;
-        sides[1] = side2;
-        sides[2] = side3;
+        double[] sides = new double[]{side1, side2, side3};
 
         Arrays.sort(sides);
         if (sides[0] <= 0) {
-            throw new TriangleException("One or more side is 0");
-        } else if ((sides[0] + sides[1] < sides[2])) {
-            throw new TriangleException("Doesn't pass the triangle inequality law");
-        } else {
-            this.isIsosceles = (sides[0] == sides[1] || sides[1] == sides[2]);
-            this.isEquilateral = (sides[0] == sides[1] && sides[1] == sides[2]);
+            throw new TriangleException("Not a triangle");
         }
+
+        if ((sides[0] + sides[1] < sides[2])) {
+            throw new TriangleException("Doesn't pass the triangle inequality law");
+        }
+
+        this.isIsosceles = (sides[0] == sides[1] || sides[1] == sides[2]);
+        this.isEquilateral = (sides[0] == sides[2]);
     }
 
     boolean isEquilateral() {
