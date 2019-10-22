d21df8b882bcb17efeaadf89e7704d445f18bbf9
mayurdw <mayurwadhwanid@gmail.com>
2019-10-22 21:14:25 +1300

    Sum of multiples new branch

---
+++ java/sum-of-multiples/src/main/java/SumOfMultiples.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class SumOfMultiples {
+    int sum = 0;
+
+    SumOfMultiples(int number, int[] set) {
+        this.sum = number;
+    }
+
+    int getSum() {
+        return this.sum;
+    }
+
+}
