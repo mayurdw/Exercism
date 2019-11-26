98eff6b2ab49d33728fb308e01db5df9ddddfa92
mayurdw <mayurwadhwanid@gmail.com>
2019-11-26 21:58:51 +1300

    Finished first edit

--- java/rotational-cipher/src/main/java/RotationalCipher.java
+++ java/rotational-cipher/src/main/java/RotationalCipher.java
@@ -4,17 +4,37 @@ class RotationalCipher {
      private final int shiftKey;
 
     RotationalCipher(int shiftKey) {
-        this.shiftKey = ( shiftKey < 0 || shiftKey > 26 ) ? 0 : shiftKey;
+        this.shiftKey = ( shiftKey < 0 || shiftKey > 25 ) ? 0 : shiftKey;
     }
 
     String rotate(String data) {
-        char[] cypher = new char[data.length()];
+        String ret;
 
-        for( int x = 0; x < data.length(); x++ ){
-            cypher[x] = (char)(data.charAt(x) - this.shiftKey);
+        if( this.shiftKey != 0 ){
+            char[] alphabets = data.toCharArray();
+
+            for( int i = 0; i < alphabets.length; i++ ){
+                if( Character.isAlphabetic(alphabets[i])) {
+                    boolean isLowerCase = Character.isLowerCase(alphabets[i]);
+                    alphabets[i] = (char) ((int) alphabets[i] + this.shiftKey);
+                    if( isLowerCase && alphabets[i] > 'z' ){
+                        alphabets[i] -= 26;
+                    }
+                    else if( !isLowerCase && alphabets[i] > 'Z' ){
+                        alphabets[i] -= 26;
+                    }
+                    else{
+                        // do nothing
+                    }
+                }
+            }
+            ret = new String(alphabets);
+        }
+        else{
+            ret = data;
         }
 
-        return Arrays.toString(cypher);
+        return ret;
     }
 
 }
