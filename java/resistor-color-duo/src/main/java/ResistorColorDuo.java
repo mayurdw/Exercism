58c6c0c5b0ff8aa69d971f94659682730a539976
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2020-04-10 11:18:25 +1200

    Resistor Color Duo Complete

--- java/resistor-color-duo/src/main/java/ResistorColorDuo.java
+++ java/resistor-color-duo/src/main/java/ResistorColorDuo.java
@@ -1,33 +1,30 @@
 class ResistorColorDuo {
-    private enum ResistorCode
-    {
-        BLACK(0, "black"),
-        BROWN( 1, "brown"),
-        RED(2, "red"),
-        ORANGE(3, "orange"),
-        YELLOW(4, "yellow"),
-        GREEN(5, "green"),
-        BLUE( 6, "blue"),
-        VIOLET(7, "violet"),
-        GREY(8, "grey"),
-        WHITE(9, "white");
+    private enum ResistorCode {
+        BLACK( 0 ),
+        BROWN( 1 ),
+        RED( 2 ),
+        ORANGE( 3 ),
+        YELLOW( 4 ),
+        GREEN( 5 ),
+        BLUE( 6 ),
+        VIOLET( 7 ),
+        GREY( 8 ),
+        WHITE( 9 );
 
-        int value;
-        String name;
+        private int value;
 
-        ResistorCode( int value, String name ){
+        ResistorCode( int value ) {
             this.value = value;
-            this.name = name;
         }
-    };
+    }
 
-    int value(String[] colors) {
+    int value( String[] colors ) {
         if( colors.length < 2 )
             return -1;
 
-        ResistorCode tensPlace = ResistorCode.valueOf(colors[0].toLowerCase());
-        ResistorCode unitsPlace = ResistorCode.valueOf(colors[1].toLowerCase());
+        ResistorCode tensPlace = ResistorCode.valueOf( colors[0].toUpperCase() );
+        ResistorCode unitsPlace = ResistorCode.valueOf( colors[1].toUpperCase() );
 
-        return ( tensPlace.value * 10 + unitsPlace.value);
+        return ( tensPlace.value * 10 + unitsPlace.value );
     }
 }
