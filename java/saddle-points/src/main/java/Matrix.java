7a064a0b32d6f1bd042611ca7d3e84e7c4ff2c2a
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2020-04-11 14:01:00 +1200

    Changes as suggested by the mentor. Hopefully they are good enough

--- java/saddle-points/src/main/java/Matrix.java
+++ java/saddle-points/src/main/java/Matrix.java
@@ -7,41 +7,74 @@ import java.util.Set;
 class Matrix {
     private List<List<Integer>> values;
     private Set<MatrixCoordinate> saddlePoints = new HashSet<>();
-    private final int NUMBER_OF_COLUMNS;
 
-    Matrix(List<List<Integer>> values) {
-        this.values = values;
-        this.NUMBER_OF_COLUMNS = ( !this.values.isEmpty() ) ? values.get(0).size() : 0;
+    Matrix( List<List<Integer>> values ) {
+        this.values = new ArrayList<>( values );
         determineSaddlePoints();
     }
 
-    private List<Integer> getColumn( Integer columnNumber ){
+    private List<Integer> getColumn( Integer columnNumber ) {
         List<Integer> column = new ArrayList<>();
 
-        if( columnNumber >= 0 && !this.values.isEmpty() && this.NUMBER_OF_COLUMNS != 0){
-            for( List<Integer> row : this.values ){
-                column.add(row.get(columnNumber));
+        if( columnNumber >= 0 && !this.values.isEmpty() ) {
+            for ( List<Integer> row : this.values ) {
+                column.add( row.get( columnNumber ) );
             }
         }
 
         return column;
     }
 
-    private void determineSaddlePoints(){
-        if( !this.values.isEmpty() ) {
-            int rowNumber = 0;
+    // Get sorted MatrixCoordinates of RowMax
+    private Set<MatrixCoordinate> getRowMaxes() {
+        Set<MatrixCoordinate> rowMaxes = new HashSet<>(  );
+        int rowNumber = 1;
 
-            for( List<Integer> row : this.values ){
-                rowNumber++;
-                if( !row.isEmpty()){
-                    final int ROW_MAX = Collections.max(row);
-                    for( int x = 0; x < row.size(); x++ ) {
-                        if ( row.get(x) == ROW_MAX && ( Collections.min(this.getColumn(x)) == row.get(x) ) ) {
-                            this.saddlePoints.add(new MatrixCoordinate(rowNumber, x + 1));
-                        }
-                    }
+        // Get a single row
+        for ( List<Integer> row : this.values ) {
+            int columnNumber = 1;
+            Integer maxValue = Collections.max( row );
+
+            for( Integer rowValue : row ){
+                if( maxValue.equals( rowValue ) ){
+                    rowMaxes.add( new MatrixCoordinate( rowNumber, columnNumber ) );
                 }
+                columnNumber++;
             }
+
+            rowNumber++;
+        }
+
+        return rowMaxes;
+    }
+
+    // Get sorted MatrixCoordinates of ColumnMin
+    private Set<MatrixCoordinate> getColMins(){
+        Set<MatrixCoordinate> colMins = new HashSet<>(  );
+        int columnNumber = 0;
+
+        for( int columnPos = 0; columnPos < this.values.get(0).size(); columnPos++ ){
+            List<Integer> col = this.getColumn( columnNumber++ );
+            Integer colMin = Collections.min( col );
+            Integer rowPos = 0;
+
+            for( Integer colValue : col ){
+                if( colMin.equals( colValue )){
+                    colMins.add( new MatrixCoordinate( rowPos + 1, columnPos + 1  ) );
+                }
+                rowPos++;
+            }
+        }
+
+        return colMins;
+    }
+
+    private void determineSaddlePoints() {
+        if( !this.values.isEmpty() ) {
+
+            this.saddlePoints = this.getRowMaxes();
+            this.saddlePoints.retainAll( this.getColMins() );
+
         }
     }
 
