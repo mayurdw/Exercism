c28438dc5f942b64f4eac3e69e4dd16b922a7466
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2020-04-12 13:53:15 +1200

    Grains exercise completed

---
+++ java/grains/src/main/java/Grains.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+
+class Grains {
+
+    BigInteger grainsOnSquare( final int square ) {
+        BigInteger value = BigInteger.ONE;
+        if( square < 1 || square > 64 )
+            throw new IllegalArgumentException( "square must be between 1 and 64" );
+
+        return value.shiftLeft( square - 1 );
+    }
+
+    BigInteger grainsOnBoard() {
+        return BigInteger.ZERO.setBit( 64 ).subtract( BigInteger.ONE );
+    }
+
+}
