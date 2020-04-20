dd52d4e29fff7b5a42654cce4f9343fdc4e82bd5
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2020-04-20 21:13:44 +1200

    Sieve completed

--- java/sieve/src/main/java/Sieve.java
+++ java/sieve/src/main/java/Sieve.java
@@ -1,11 +1,32 @@
-import java.util.List;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 class Sieve {
-    Sieve(int maxPrime) {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+    Integer maxPrime;
+    List<Integer> primes = new ArrayList<>();
+
+    Sieve( int maxPrime ) {
+        this.maxPrime = maxPrime;
+        calculatePrime();
+    }
+
+    private void calculatePrime() {
+        List<Integer> candidates = Arrays.stream( IntStream.rangeClosed( 2, this.maxPrime ).toArray() ).boxed().collect( Collectors.toList() );
+
+        while ( !candidates.isEmpty() ) {
+            Integer next = candidates.get( 0 );
+            candidates.remove( next );
+            this.primes.add( next );
+            Predicate<Integer> divisible = d -> d % next == 0;
+
+            candidates.removeIf( divisible );
+        }
+
     }
 
     List<Integer> getPrimes() {
-        throw new UnsupportedOperationException("Delete this statement and provide your own implementation.");
+        return this.primes;
     }
 }
