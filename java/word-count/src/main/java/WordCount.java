8cd94a481eca2135607768dadc9e73aafc430e93
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2020-04-25 20:55:00 +1200

    All tests pass

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -3,12 +3,14 @@ import java.util.Map;
 
 public class WordCount {
     public Map<String, Integer> phrase( String sentence ) {
-        String[] words = sentence.split( "[^a-zA-Z\\d]" );
+        String[] words = sentence.split( "[\\W&&[^']]" );
         Map<String, Integer> wordCount = new HashMap<>( );
 
         for( String word : words ){
-            System.out.println( "Word = " + word );
             if( word.length() > 0 ) {
+                if( word.indexOf( "'" ) == 0 && ( word.lastIndexOf( "'" ) == word.length() - 1 ) ) {
+                    word = word.substring( 1, word.length() - 1 );
+                }
                 wordCount.merge( word.toLowerCase(), 1, Integer::sum );
             }
         }
