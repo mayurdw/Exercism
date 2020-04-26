89f2b614508cd3641031424f789f2a3838176861
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2020-04-26 18:24:34 +1200

    Twelve Days done

--- java/twelve-days/src/main/java/TwelveDays.java
+++ java/twelve-days/src/main/java/TwelveDays.java
@@ -1,13 +1,69 @@
 class TwelveDays {
-    String verse(int verseNumber) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    private final String DAYS[] =
+            {
+                    "first", "second", "third", "fourth",
+                    "fifth", "sixth", "seventh", "eighth",
+                    "ninth", "tenth", "eleventh", "twelfth"
+            };
+    private final String FORMAT = "On the %s day of Christmas my true love gave to me: ";
+    private final String OBJECTS[] =
+            {
+                    "a Partridge in a Pear Tree.",
+                    "two Turtle Doves, ",
+                    "three French Hens, ",
+                    "four Calling Birds, ",
+                    "five Gold Rings, ",
+                    "six Geese-a-Laying, ",
+                    "seven Swans-a-Swimming, ",
+                    "eight Maids-a-Milking, ",
+                    "nine Ladies Dancing, ",
+                    "ten Lords-a-Leaping, ",
+                    "eleven Pipers Piping, ",
+                    "twelve Drummers Drumming, "
+            };
+
+    String verse( int verseNumber ) {
+        if( ( verseNumber - 1 ) >= OBJECTS.length || ( verseNumber - 1 ) >= DAYS.length )
+            throw new IllegalArgumentException( "verseNumber exceeds expectations" );
+
+        String verseString = String.format( FORMAT, DAYS[verseNumber - 1] );
+
+        switch ( verseNumber ){
+            case 12: verseString += OBJECTS[11];
+            case 11: verseString += OBJECTS[10];
+            case 10: verseString += OBJECTS[9];
+            case 9: verseString += OBJECTS[8];
+            case 8: verseString += OBJECTS[7];
+            case 7: verseString += OBJECTS[6];
+            case 6: verseString += OBJECTS[5];
+            case 5: verseString += OBJECTS[4];
+            case 4: verseString += OBJECTS[3];
+            case 3: verseString += OBJECTS[2];
+            case 2: verseString += OBJECTS[1];
+                    verseString += "and ";
+            case 1:
+            default: verseString += OBJECTS[0]; break;
+        }
+
+        verseString += "\n";
+
+        return verseString;
     }
 
-    String verses(int startVerse, int endVerse) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    String verses( int startVerse, int endVerse ) {
+        StringBuilder result = new StringBuilder(  );
+
+        for( int x = startVerse; x <= endVerse; x++ ){
+            result.append( this.verse( x ) );
+            result.append( "\n" );
+        }
+
+        result.deleteCharAt( result.length() - 1 );
+
+        return result.toString();
     }
-    
+
     String sing() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return this.verses( 1, 12 );
     }
 }
--- java/twelve-days/src/test/java/TwelveDaysTest.java
+++ java/twelve-days/src/test/java/TwelveDaysTest.java
@@ -19,7 +19,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseOne, twelveDays.verse(1));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseTwo() {
         String expectedVerseTwo = "On the second day of Christmas my true love gave to me: two Turtle Doves, " +
@@ -27,7 +26,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseTwo, twelveDays.verse(2));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseThree() {
         String expectedVerseThree = "On the third day of Christmas my true love gave to me: three French Hens, " +
@@ -35,7 +33,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseThree, twelveDays.verse(3));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseFour() {
         String expectedVerseFour = "On the fourth day of Christmas my true love gave to me: four Calling Birds, " +
@@ -43,7 +40,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseFour, twelveDays.verse(4));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseFive() {
         String expectedVerseFive = "On the fifth day of Christmas my true love gave to me: five Gold Rings, " +
@@ -51,7 +47,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseFive, twelveDays.verse(5));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseSix() {
         String expectedVerseSix = "On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, " +
@@ -60,7 +55,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseSix, twelveDays.verse(6));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseSeven() {
         String expectedVerseSeven = "On the seventh day of Christmas my true love gave to me: " +
@@ -69,7 +63,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseSeven, twelveDays.verse(7));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseEight() {
         String expectedVerseEight = "On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking," +
@@ -78,7 +71,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseEight, twelveDays.verse(8));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseNine() {
         String expectedVerseNine = "On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, " +
@@ -87,7 +79,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseNine, twelveDays.verse(9));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseTen() {
         String expectedVerseTen = "On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, " +
@@ -97,7 +88,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseTen, twelveDays.verse(10));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseEleven() {
         String expectedVerseEleven = "On the eleventh day of Christmas my true love gave to me: " +
@@ -107,7 +97,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseEleven, twelveDays.verse(11));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testVerseTwelve() {
         String expectedVerseTwelve = "On the twelfth day of Christmas my true love gave to me: " +
@@ -117,7 +106,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVerseTwelve, twelveDays.verse(12));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testFirstThreeVerses() {
         String expectedVersesOneToThree = "On the first day of Christmas my true love gave to me: " +
@@ -129,7 +117,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVersesOneToThree, twelveDays.verses(1, 3));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testFourthToSixthVerses() {
         String expectedVersesFourToSix = "On the fourth day of Christmas my true love gave to me: " + 
@@ -141,7 +128,6 @@ public class TwelveDaysTest {
         assertEquals(expectedVersesFourToSix, twelveDays.verses(4, 6));
     }
 
-    @Ignore("Remove to run test")
     @Test
     public void testSingWholeSong() {
         String expectedSong = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n" +
