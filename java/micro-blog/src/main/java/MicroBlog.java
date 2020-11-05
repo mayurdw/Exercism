811657d7559c4b0b859871f9a0375b313f57dd17
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2020-11-05 19:30:08 +1300

    Build gradle file needed to run tests
    Example code used to pass tests

--- java/micro-blog/src/main/java/MicroBlog.java
+++ java/micro-blog/src/main/java/MicroBlog.java
@@ -3,9 +3,12 @@ import java.util.Arrays;
 
 class MicroBlog {
     public String truncate( String input ) {
-        byte[] encoded_input = StandardCharsets.UTF_8.encode( input ).array();
-        int maxLen = Math.min( encoded_input.length, 5 );
-
-        return new String( Arrays.copyOf(encoded_input, maxLen), StandardCharsets.UTF_8 );
+        return input.codePoints()
+                .limit(5)
+                .collect(
+                        StringBuilder::new,
+                        StringBuilder::appendCodePoint,
+                        StringBuilder::append)
+                .toString();
     }
 }
