a9c2680328b38e9ceba92240826a70b136c315ee
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2020-11-05 20:57:14 +1300

    Error handling partially done

---
+++ java/error-handling/src/main/java/ErrorHandling.java
@@ -0,0 +1,49 @@
+import java.util.Optional;
+
+class ErrorHandling {
+
+    void handleErrorByThrowingIllegalArgumentException() {
+        throw new IllegalArgumentException();
+    }
+
+    void handleErrorByThrowingIllegalArgumentExceptionWithDetailMessage(String message) {
+        throw new IllegalArgumentException("This is the detail message.");
+    }
+
+    void handleErrorByThrowingAnyCheckedException() throws CustomCheckedException {
+        throw new CustomCheckedException();
+    }
+
+    void handleErrorByThrowingAnyCheckedExceptionWithDetailMessage(String message) throws CustomCheckedException {
+        throw new CustomCheckedException("This is the detail message.");
+    }
+
+    void handleErrorByThrowingAnyUncheckedException() {
+        // Delete this statement and write your own implementation.
+    }
+
+    void handleErrorByThrowingAnyUncheckedExceptionWithDetailMessage(String message) {
+        // Delete this statement and write your own implementation.
+    }
+
+    void handleErrorByThrowingCustomCheckedException() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    void handleErrorByThrowingCustomCheckedExceptionWithDetailMessage(String message) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    void handleErrorByThrowingCustomUncheckedException() {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    void handleErrorByThrowingCustomUncheckedExceptionWithDetailMessage(String message) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+    Optional<Integer> handleErrorByReturningOptionalInstance(String integer) {
+        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    }
+
+}
