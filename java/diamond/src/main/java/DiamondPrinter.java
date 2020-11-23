cbd92f3fd6b68afc199a494e3d097100cf5b8312
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2020-11-23 20:31:33 +1300

    Diamond started

---
+++ java/diamond/src/main/java/DiamondPrinter.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class DiamondPrinter {
+
+    List<String> printToList(char a) {
+        List<String> stringList = new ArrayList<>();
+        if( Character.isAlphabetic( a ) ) {
+            final int NUMBER_OF_ROWS = ( Character.toUpperCase( a ) - 'A' * 2 ) - 1;
+            
+        }
+        return stringList;
+    }
+
+}
