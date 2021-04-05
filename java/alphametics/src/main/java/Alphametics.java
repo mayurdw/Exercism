23e89adbab9d138ae0e5378357e6dd6d01a02c75
Mayur Wadhwani <mayurwadhwanid@gmail.com>
2021-04-05 18:56:32 +1200

    Started working on the problem
    TODO: Extract list of characters
    
    TODO: Calculate a numeric to each

--- java/alphametics/src/main/java/Alphametics.java
+++ java/alphametics/src/main/java/Alphametics.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
+import java.util.List;
 
 public class Alphametics {
     private final String equation;
@@ -6,10 +9,41 @@ public class Alphametics {
         this.equation = equation;
     }
 
+    /**
+     * Splits {@link Alphametics#equation} to Sum and its parts
+     * @param parts {@link List} of Strings containing parts
+     * @return sum {@link String} of the equation
+     * */
+    private String findSumAndParts( List<String> parts ){
+        String sum = null;
+
+        if( parts == null ){
+            parts = new ArrayList<>();
+        }
+        final String EQUALS = " == ";
+        int index = this.equation.indexOf( EQUALS );
+
+        if( index < this.equation.length() && index >= 0 ){
+            sum = this.equation.substring( index + EQUALS.length() );
+            String part = this.equation.substring( 0, index );
+
+            String[] splitParts = part.split( " \\+ " );
+
+            parts.addAll( Arrays.asList( splitParts ) );
+        }
+
+        return sum;
+    }
+
     LinkedHashMap<Character, Integer> solve() throws UnsolvablePuzzleException{
-        if( !this.equation.contains( "+" ) ){
+        if( !this.equation.contains( " + " ) && !this.equation.contains( " == " ) ){
             throw new UnsolvablePuzzleException( "Not an equation" );
         }
+
+        List<String> additions = new ArrayList<>();
+        String sum = this.findSumAndParts( additions );
+
+
 
         return null;
     }
